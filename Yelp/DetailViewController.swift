456286bab957226e75f1f4bb58a0652904bc48f6
Julia Yu <juliayu@gmail.com>
2016-02-20 20:23:20 -0800

    save layout

--- Yelp/DetailViewController.swift
+++ Yelp/DetailViewController.swift
@@ -19,45 +19,7 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var detailCategories: UILabel!
     @IBOutlet weak var detailReviewCount: UILabel!
     
-    var business: Business? {
-        didSet {
-
-//            if let business = self.business {
-//                
-//                if let url = business.imageURL {
-//                    print("wtf is this img url", url)
-//                    print(self.detailImage)
-//                    
-////                    self.detailImage.setImageWithURL(url)
-//                }
-//                
-//            }
-            
-//            if let imageURL = self.business?.imageURL {
-//                self.detailImage.setImageWithURL(imageURL)
-//            }
-//            
-//            if let imageURL = self.business?.ratingImageURL {
-//                self.detailRating.setImageWithURL(imageURL)
-//            }
-//            
-//            if let distance = self.business?.distance {
-//                self.detailDistance.text = distance
-//            }
-//            
-//            if let count = self.business?.reviewCount {
-//                self.detailReviewCount.text = "\(count) Reviews"
-//            }
-//            
-//            if let address = self.business?.address {
-//                self.detailAddress.text = address
-//            }
-//            
-//            if let cat = self.business?.categories {
-//                self.detailCategories.text = cat
-//            }
-        }
-    }
+    var business: Business?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -65,20 +27,38 @@ class DetailViewController: UIViewController {
         self.scrollView.contentSize = CGSize(width: self.view.frame.width, height: self.view.frame.height*1.6)
         
         if let biz = self.business {
+            
             if let url = biz.imageURL {
-                
                 var stringURL = String(url)
                 stringURL = stringURL.stringByReplacingOccurrencesOfString("ms.jpg", withString: "o.jpg")
                 let newURL = NSURL(string: stringURL)
-              
                 self.detailImage.setImageWithURL(newURL!)
             }
             
-        }
-        
-        
+            if let name = biz.name {
+                self.detailName.text = name
+            }
+            
+            if let ratingUrl = biz.ratingImageURL {
+                self.detailRating.setImageWithURL(ratingUrl)
+            }
 
-        // Do any additional setup after loading the view.
+            if let distance = biz.distance {
+                self.detailDistance.text = distance
+            }
+
+            if let count = biz.reviewCount {
+                self.detailReviewCount.text = "\(count) Reviews"
+            }
+
+            if let address = biz.address {
+                self.detailAddress.text = address
+            }
+
+            if let cat = biz.categories {
+                self.detailCategories.text = cat
+            }
+        }
     }
 
 }
