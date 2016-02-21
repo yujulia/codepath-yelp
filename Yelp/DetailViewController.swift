6c7374667bbb7cfa1fed68de0c31fb2e273bc5a4
Julia Yu <juliayu@gmail.com>
2016-02-20 20:15:20 -0800

    layout detail

--- Yelp/DetailViewController.swift
+++ Yelp/DetailViewController.swift
@@ -10,24 +10,73 @@ import UIKit
 
 class DetailViewController: UIViewController {
     
+    @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var detailImage: UIImageView!
     @IBOutlet weak var detailName: UILabel!
     @IBOutlet weak var detailRating: UIImageView!
-    @IBOutlet weak var detailReviewImage: UILabel!
     @IBOutlet weak var detailDistance: UILabel!
     @IBOutlet weak var detailAddress: UILabel!
     @IBOutlet weak var detailCategories: UILabel!
+    @IBOutlet weak var detailReviewCount: UILabel!
     
     var business: Business? {
         didSet {
-            print("got info about business")
+
+//            if let business = self.business {
+//                
+//                if let url = business.imageURL {
+//                    print("wtf is this img url", url)
+//                    print(self.detailImage)
+//                    
+////                    self.detailImage.setImageWithURL(url)
+//                }
+//                
+//            }
             
-            print(business)
+//            if let imageURL = self.business?.imageURL {
+//                self.detailImage.setImageWithURL(imageURL)
+//            }
+//            
+//            if let imageURL = self.business?.ratingImageURL {
+//                self.detailRating.setImageWithURL(imageURL)
+//            }
+//            
+//            if let distance = self.business?.distance {
+//                self.detailDistance.text = distance
+//            }
+//            
+//            if let count = self.business?.reviewCount {
+//                self.detailReviewCount.text = "\(count) Reviews"
+//            }
+//            
+//            if let address = self.business?.address {
+//                self.detailAddress.text = address
+//            }
+//            
+//            if let cat = self.business?.categories {
+//                self.detailCategories.text = cat
+//            }
         }
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.scrollView.contentSize = CGSize(width: self.view.frame.width, height: self.view.frame.height*1.6)
+        
+        if let biz = self.business {
+            if let url = biz.imageURL {
+                
+                var stringURL = String(url)
+                stringURL = stringURL.stringByReplacingOccurrencesOfString("ms.jpg", withString: "o.jpg")
+                let newURL = NSURL(string: stringURL)
+              
+                self.detailImage.setImageWithURL(newURL!)
+            }
+            
+        }
+        
+        
 
         // Do any additional setup after loading the view.
     }
